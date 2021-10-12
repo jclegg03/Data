3626377a095b6e4c0cda6b653f7ec8ac2326456a
jclegg03 <jjc2003@comcast.net>
2021-10-12 11:59:24 -0600

    Made my data project

---
+++ Data/Resources/DataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataApp.swift
+//  Data
+//
+//  Created by Clegg, Jay on 10/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
