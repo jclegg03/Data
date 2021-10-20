c150434d021ea81a214322380009ec1ab04c5b05
jclegg03 <jjc2003@comcast.net>
2021-10-20 13:40:08 -0600

    added the bucketrow view

---
+++ Data/ViewController/BucketRowView.swift
@@ -0,0 +1,28 @@
+//
+//  BucketRowView.swift
+//  Data
+//
+//  Created by Clegg, Jay on 10/20/21.
+//
+
+import SwiftUI
+
+struct BucketRowView: View
+{
+    var body: some View
+    {
+        HStack
+        {
+            Text("Text1")
+            Text("Text2")
+        }
+    }
+}
+
+struct BucketRowView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        BucketRowView()
+    }
+}
