ebc585db9091ec3bbb1bd97ede530515aa443154
jclegg03 <jjc2003@comcast.net>
2021-10-26 13:36:26 -0600

    added the random emoji function

--- Data/Model/Utilities.swift
+++ Data/Model/Utilities.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+func generateRandomEmoji(of emojiType: String) -> String
+{
+    let start : Int
+    let range : Int
+    
+    if (emojiType == "face")
+    {
+        start = 0x1F601
+        range = 52
+    }
+    else if (emojiType == "symbol")
+    {
+        start = 0x1F680
+        range = 52
+    }
+    else if (emojiType == "greek")
+    {
+        start = 0x03B1
+        range = 24
+    }
+    else
+    {
+        start = 0x1F400
+        range = 52
+    }
+    
+    let unicodeValue = start + Int(arc4random_uniform(UInt32(range)))
+    let currentEmoji = UnicodeScalar(unicodeValue)?.description ?? "👹"
+    
+    return currentEmoji
+}
