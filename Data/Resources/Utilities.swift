7c04b3b7bb14242b76489a5df82da883efc4b008
jclegg03 <jjc2003@comcast.net>
2021-11-01 12:20:47 -0600

    added the demos for each type and also fixed the decode method

--- Data/Resources/Utilities.swift
+++ Data/Resources/Utilities.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 let demoItem : BucketListItem = BucketListItem(year: -9395891, goal: "My goal is to annoy someone here", creature: "Creature the house elf")
+let secondDemo: ElectionDatum = ElectionDatum(state: "Utah", cantidateName: "Donald John Trump", sampleSize: 13463, weight: 0.003, influence: 0.6, percent: 0.613, houseAdjustedPercent: 0.134714, trendAndHouseAdjustedPercent: 0.5711443)
+let thirdDems: VoterRegistration = VoterRegistration(Jurisdiction: "Utah", Year: 2002, Month: "January", NewRegisteredVoters: 116413)
 
 func generateRandomEmoji(of emojiType: String) -> String
 {
@@ -41,7 +43,7 @@ func generateRandomEmoji(of emojiType: String) -> String
     return currentEmoji
 }
 
-func loadJSON (from file : String) -> [BucketListItem]
+func loadJSON (from file : String) -> [Any]
 {
     if let dataURL = Bundle.main.url(forResource: file, withExtension: "json")
     {
@@ -49,32 +51,26 @@ func loadJSON (from file : String) -> [BucketListItem]
         let decoder = JSONDecoder()
         do
         {
-            let results = try decoder.decode([BucketListItem].self, from: data)
-            return results
+            if(file == "bucket2021")
+            {
+                let results = try decoder.decode([BucketListItem].self, from: data)
+                return results
+            }
+            else if(file == "PresidentialPolls")
+            {
+                let results = try decoder.decode([ElectionDatum].self, from: data)
+                return results
+            }
+            else if(file == "VoterRegistration")
+            {
+                let results = try decoder.decode([VoterRegistration].self, from: data)
+                return results
+            }
         }
         catch
         {
             print(error.localizedDescription)
         }
     }
-    return [BucketListItem]() // TOTAL DATA READ FAILURE!
-}
-
-func loadJSON (from file : String) -> [ElectionDatum]
-{
-    if let dataURL = Bundle.main.url(forResource: file, withExtension: "json")
-    {
-        let data = try! Data(contentsOf: dataURL)
-        let decoder = JSONDecoder()
-        do
-        {
-            let results = try decoder.decode([ElectionDatum].self, from: data)
-            return results
-        }
-        catch
-        {
-            print(error.localizedDescription)
-        }
-    }
-    return [ElectionDatum]() // TOTAL DATA READ FAILURE!
+    return [Any]() // TOTAL DATA READ FAILURE!
 }
