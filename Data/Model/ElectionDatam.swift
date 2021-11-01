95ca9aa79ae37168451092077c21cea2f3f3c633
jclegg03 <jjc2003@comcast.net>
2021-11-01 11:35:01 -0600

    Renamed variables to cammelCase

--- Data/Model/ElectionDatam.swift
+++ Data/Model/ElectionDatam.swift
@@ -11,11 +11,11 @@ struct ElectionDatum: Codable, Identifiable
 {
     var id = UUID().uuidString
     var state: String
-    var cantidate_name: String
-    var samplesize: Int
+    var cantidateName: String
+    var sampleSize: Int
     var weight: Double
     var influence: Double
-    var pct: Double
-    var house_adjusted_pct: Double
-    var trend_and_house_adjusted_pct: Double
+    var percent: Double
+    var houseAdjustedPercent: Double
+    var trendAndHouseAdjustedPercent: Double
 }
